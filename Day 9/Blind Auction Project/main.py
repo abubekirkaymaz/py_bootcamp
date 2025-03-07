bc33a78de12f383aec93530fcc90a025bb952062
Bekir <abubekirkaymaz@gmail.com>
2025-03-07 03:13:09 +0300

    Merge remote-tracking branch 'origin/main'

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,36 @@
+import art
 # TODO-1: Ask the user for input
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
 
+print(art.logo)
+
+bid_dict = {}
+
+continue_result = True
+while continue_result:
+
+    name = input("what is your name?: ").lower()
+    price = int(input("what is your bid: $ "))
+
+    bid_dict[name] = price
+
+    answer = input("Are there any other bidders? Type 'yes or 'no'. \n").lower()
+
+    if answer == "no":
+        continue_result = False
+    elif answer == "yes":
+        print("\n" * 100)
+        print("")
+
+max_bid = 0
+win_name = ""
+
+for name in bid_dict:
+    if bid_dict[name] > max_bid:
+        max_bid = bid_dict[name]
+        win_name = name
+
+print(f"The winner is {win_name} with a bid of $ {bid_dict[win_name]}")
 
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,24 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades = {}
+
+for name in student_scores:
+    if student_scores[name] <= 70:
+        student_grades[name] = "Fail"
+
+    elif student_scores[name] >= 71 and student_scores[name] <= 80:
+        student_grades[name] = "Acceptable"
+
+    elif student_scores[name] >= 81 and student_scores[name] <= 90:
+        student_grades[name] = "Acceptable"
+
+    elif student_scores[name] >= 91 and student_scores[name] <= 100:
+        student_grades[name] = "Acceptable"
+
+print(student_grades)
