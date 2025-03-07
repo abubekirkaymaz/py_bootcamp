4ffa5e71cb89ac96c8d8780f2372c64f74803fbf
Bekir <abubekirkaymaz@gmail.com>
2025-03-08 01:12:11 +0300

    Merge remote-tracking branch 'origin/main'

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -5,7 +5,7 @@ print(logo)
 def add(n1, n2):
     return n1 + n2
 
-def subtrack(n1, n2):
+def subtract(n1, n2):
     return n1 - n2
 
 def multiply(n1, n2):
@@ -16,36 +16,31 @@ def divide(n1, n2):
 
 calc_dict = {
     "+": add,
-    "-": subtrack,
+    "-": subtract,
     "*": multiply,
     "/": divide
-    }
+}
 
-ongoing_process = False
-result = None
-to_continue = True
-
-while to_continue:
-    if not ongoing_process:
+def calculator():
+    while True:
         first_number = int(input("What is the first number: "))
-    else:
-        first_number = result
 
-    print("+\n-\n*\n/")
-    math_operator = input("Pick an operation: ")
-    second_number = int(input("What's the next number?: "))
-    result = calc_dict[math_operator](first_number, second_number)
-    print( f"{first_number} {math_operator} {second_number} = {result}")
-    ask_continue = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation (or 'q' to quit): ").lower()
+        while True:
+            print("+\n-\n*\n/")
+            math_operator = input("Pick an operation: ")
+            second_number = int(input("What's the next number?: "))
 
-    if ask_continue == "y":
-        ongoing_process = True
+            result = calc_dict[math_operator](first_number, second_number)
+            print(f"{first_number} {math_operator} {second_number} = {result}")
 
-    elif ask_continue == "n":
-        ongoing_process = False
+            ask_continue = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation (or 'q' to quit): ").lower()
 
-    elif ask_continue == "q":
-        print("**************************Finish Calculate*********************************")
-        to_continue = False
+            if ask_continue == "y":
+                first_number = result
+            elif ask_continue == "n":
+                break  #
+            elif ask_continue == "q":
+                print("*********************Exit Program*******************************")
+                return
 
-
+calculator()
